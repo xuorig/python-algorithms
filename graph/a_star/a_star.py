9d1eb338568cfee248a7c518cf041f38ee1688d4
Marc-André Giroux <mgiroux0@gmail.com>
2015-10-23 19:18:44 -0400

    comments

--- graph/a_star/a_star.py
+++ graph/a_star/a_star.py
@@ -2,8 +2,9 @@ from utils.priority_queue import PriorityQueue
 from graph.a_star.a_star_grid import AStarGrid
 from collections import defaultdict
 
+# Reconstruct the path from start to goal using
+# the
 def reconstruct_path(came_from, start, goal):
-    import pdb; pdb.set_trace()
     current = goal
     path = [current]
     while current != start:
@@ -13,19 +14,21 @@ def reconstruct_path(came_from, start, goal):
     path.reverse()
     return path
 
+# Manhattan Distance
+# https://en.wikipedia.org/wiki/Taxicab_geometry
 def heuristic(node, goal):
     dx = abs(node[0] - goal[0])
     dy = abs(node[1] - goal[1])
     return 1 * (dx + dy)
 
 def a_star(grid, start, goal):
-    # Manhattan Distance
-    # https://en.wikipedia.org/wiki/Taxicab_geometry
-
+    # Initialize an open and closed set.
+    # The open queue is a priority queue
     open_queue = PriorityQueue()
     open_queue.put(start, 0)
     closed = set()
 
+    # g_score and f_score are dictionaries with infinity default values
     came_from = {}
     g_score = defaultdict(lambda: float('inf'))
     g_score[start] = 0
@@ -37,7 +40,7 @@ def a_star(grid, start, goal):
         # Pop the node with the lowest f score
         current = open_queue.get()
         closed.add(current)
-        print current
+
         # If we hit our goal, return the path
         if current == goal:
             return reconstruct_path(came_from, start, goal)
@@ -46,18 +49,23 @@ def a_star(grid, start, goal):
             if neighbor in closed:
                 continue
 
+            # Calculate g score, heursitic and f score for the neighbor
             g = g_score[current] + grid.dist_between(current, neighbor)
             h = heuristic(neighbor, goal)
             f = g + h
 
             if neighbor not in open_queue:
                 open_queue.put(neighbor, f)
+            # If the g score is bigger than what we already have, skip it.
             elif g >= g_score[neighbor]:
                 continue
 
+            # update came from, g and f score
             came_from[neighbor] = current
             g_score[neighbor] = g
             f_score[neighbor] = f
+
+    # Found no path
     return None
 
 grid = AStarGrid(4, 4)
